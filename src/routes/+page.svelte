6623b191402a7ebbccc90888bed4eb6dd9242154
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-05-10 16:16:18 +0200

    fix: home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,18 @@
+<script>
+  import { Alert } from "flowbite-svelte";
+  import { InfoCircleSolid } from "flowbite-svelte-icons";
+</script>
+
+
 <div class="p-8 max-w-4xl mx-auto">
 <h1 class="text-3xl">
   Flowbite-Svelte GitHub Issue Template
 </h1>
 
+<Alert>
+  {#snippet icon()}<InfoCircleSolid class="h-5 w-5" />{/snippet}
+  <span class="font-medium">Testing alert</span>
+  Lorem ipsum dolor sit amet consectetur.
+</Alert>
 <p>Run pnpm update and then add your code here</p>
 </div>
